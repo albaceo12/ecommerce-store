80b72edfdc49a5938a056883d389345199595b6a
albaceo12 <h.nouri1999@gmail.com>
2025-09-28 23:17:18 +0330

    backend url updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+    import.meta.mode === "development"
+      ? "http://localhost:5000/api"
+      : "https://ecommerce-store-backend-qed4.onrender.com/api",
   withCredentials: true, // send cookies to the server
 });
 
